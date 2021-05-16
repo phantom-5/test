c891f7fcd059094bb9209b8ad766f9460afc295d
phantom-5 <rickrudra@gmail.com>
2021-05-16 19:11:17 +0530

    no portrait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myapp';
+
+  isPortrait(): boolean{
+    return window.innerHeight > window.innerWidth;
+  }
+  
+  ngOnInit(): void{
+    if (this.isPortrait()){
+    var swalAlert
+    const checkPhone = async()=>{
+        swalAlert = await Swal.fire({
+        icon: 'info',
+        title: 'Hello User!',
+        text: 'This site is not designed for use in mobiles/portrait. Open in desktop for best experience.'
+      })
+    }
+     checkPhone()
+    }
+  }
 }
